b8a773e64673632188770d0c0e9593d1fd9af13f
Suhrobweb1994 <112156443+Suhrobweb1994@users.noreply.github.com>
2024-08-20 17:10:17 +0500

    Add files via upload

---
+++ arxiv.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="client/css/style.css">
+    <title>Online Zakovat</title>
+
+</head>
+<body>
+<section class="directions">
+    <div class="container m-auto">
+    <div class="double__title">
+        <h4 class="section__title aos-init" data-aos="fade-right" data-aos-duration="500">
+        O'tgan o'yindagi savollar  hali shakillanmagan
+        </h4>
+        O'tgan o'yindagi savollar  hali shakillanmagan
+        <span class="ninja__title">        
+        </span>
+      </div>
+
+            </div>
+          
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="client/css/style.css">
+    <title>Online Zakovat</title>
+
+</head>
+<body>
+<section class="directions">
+    <div class="container m-auto">
+      <div class="double__title">
+        <h4 class="section__title aos-init" data-aos="fade-right" data-aos-duration="500">
+        Online Zakovat o'yini
+        </h4>
+        <span class="ninja__title">        Online Zakovat o'yini
+        </span>
+      </div>
+<div class="grid ml-5">
+<div class='item aos-init' data-aos='fade-up' data-aos-duration='300'>
+              <div class='item__header'>
+                <div class='item__logo'>
+        
+                </div>
+                <div class='item__flexer'>
+                <form action='qatnashuvchi.php' method='post'>
+                <input type='hidden' value='{$row['id']}' name='idf'>
+                                <button class='item__name' style='background-color:transparent' type='submit'>Ishtirokchi</button>
+                </form>
+                  <button >
+                  <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor"  style='border-radius:50%' class="bi bi-person-add" viewBox="0 0 16 16">
+  <path d="M12.5 16a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7m.5-5v1h1a.5.5 0 0 1 0 1h-1v1a.5.5 0 0 1-1 0v-1h-1a.5.5 0 0 1 0-1h1v-1a.5.5 0 0 1 1 0m-2-6a3 3 0 1 1-6 0 3 3 0 0 1 6 0M8 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4"/>
+  <path d="M8.256 14a4.5 4.5 0 0 1-.229-1.004H3c.001-.246.154-.986.832-1.664C4.484 10.68 5.711 10 8 10q.39 0 .74.025c.226-.341.496-.65.804-.918Q8.844 9.002 8 9c-5 0-6 3-6 4s1 1 1 1z"/>
+</svg>
+                  </button>
+                </div>
+              </div> <ul class='item__list'>
+
+            
+           <li>Batafsil <span class='num'> 1</span>  </ul>
+              <img width='289px' height='299' src='client/img/logo/round.svg' alt='' class='item__back'>
+            </div>
+       
+<div class='item aos-init' data-aos='fade-up' data-aos-duration='300'>
+              <div class='item__header'>
+                <div class='item__logo'>
+                 <!-- {$row['logo']} -->
+                </div>
+                <div class='item__flexer'>
+                <form action='kuzatuvchi.php' method='post'>
+                <input type='hidden' value='{$row['id']}' name='idf'>
+                                <button class='item__name' style='background-color:transparent' type='submit'>Kuzatuvchi</button>
+                </form>
+                  <button >
+                  <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-people" viewBox="0 0 16 16">
+  <path d="M15 14s1 0 1-1-1-4-5-4-5 3-5 4 1 1 1 1zm-7.978-1L7 12.996c.001-.264.167-1.03.76-1.72C8.312 10.629 9.282 10 11 10c1.717 0 2.687.63 3.24 1.276.593.69.758 1.457.76 1.72l-.008.002-.014.002zM11 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4m3-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0M6.936 9.28a6 6 0 0 0-1.23-.247A7 7 0 0 0 5 9c-4 0-5 3-5 4q0 1 1 1h4.216A2.24 2.24 0 0 1 5 13c0-1.01.377-2.042 1.09-2.904.243-.294.526-.569.846-.816M4.92 10A5.5 5.5 0 0 0 4 13H1c0-.26.164-1.03.76-1.724.545-.636 1.492-1.256 3.16-1.275ZM1.5 5.5a3 3 0 1 1 6 0 3 3 0 0 1-6 0m3-2a2 2 0 1 0 0 4 2 2 0 0 0 0-4"/>
+</svg>
+                  </button>
+                </div>
+              </div> <ul class='item__list'>
+
+         
+                             
+            
+           <li>Batafsil <span class='num'> 2</span>  </ul>
+              <img width='289px' height='299' src='client/img/logo/round.svg' alt='' class='item__back'>
+            </div>
+
+                 
+<div class='item aos-init' data-aos='fade-up' data-aos-duration='300'>
+              <div class='item__header'>
+                <div class='item__logo'>
+                 <!-- {$row['logo']} -->
+                </div>
+                <div class='item__flexer'>
+                <form action='arxiv.php' method='post'>
+                <input type='hidden' value='{$row['id']}' name='idf'>
+                                <button class='item__name' style='background-color:transparent' type='submit'>O'tgan o'yindagi Savollar</button>
+                </form>
+                  <button >
+                  <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-body-text" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M0 .5A.5.5 0 0 1 .5 0h4a.5.5 0 0 1 0 1h-4A.5.5 0 0 1 0 .5m0 2A.5.5 0 0 1 .5 2h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5m9 0a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5m-9 2A.5.5 0 0 1 .5 4h3a.5.5 0 0 1 0 1h-3a.5.5 0 0 1-.5-.5m5 0a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5m7 0a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1h-3a.5.5 0 0 1-.5-.5m-12 2A.5.5 0 0 1 .5 6h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5m8 0a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5m-8 2A.5.5 0 0 1 .5 8h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5m7 0a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5m-7 2a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 0 1h-8a.5.5 0 0 1-.5-.5m0 2a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5m0 2a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5"/>
+</svg>
+                  </button>
+                </div>
+              </div> <ul class='item__list'>
+
+         
+            
+           <li>Batafsil <span class='num'> 3</span>  </ul>
+              <img width='289px' height='299' src='client/img/logo/round.svg' alt='' class='item__back'>
+            </div>
+            </div>
+          
+</body>
+</html>
